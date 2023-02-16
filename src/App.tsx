4c0c0566585f572dbe3ce41fdd4a690449f5641c
miyamoto <miyamoto.git@gmail.com>
2023-02-17 08:20:05 +0900

    ボタンを押下するとテキストエリアへ入力された文字列がログエリアへ移動する処理を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef, useEffect } from 'react';
+import { useHotkeys } from 'react-hotkeys-hook'
 
 function App() {
+  const [logs, setLogs] = useState<string[]>([]);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const scrollEndRef = useRef<HTMLDivElement>(null);
+  const ulRef = useRef<HTMLUListElement>(null);
+
+  //チャット送信のショートカットキーを設定
+  useHotkeys('ctrl+enter', addLog, { enableOnFormTags: ['textarea'] });
+
+  //textareaの値をログに加える
+  function addLog(){
+    if(textareaRef.current !== null){
+      const text = textareaRef.current.value;
+      if(text !== ''){
+        textareaRef.current.value = '';
+        setLogs([
+          ...logs,
+          text
+        ]);
+      }
+    }
+  }
+
+  useEffect(()=>{
+    //ログ画面を一番下までスクロール
+    scrollEndRef.current?.scrollIntoView()
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-log">
+        <ul ref={ulRef}>
+          { logs.map((value, index) => {
+              return(
+                <li key={index}>{value}</li>
+              )
+          })}
+        </ul>
+        <div ref={scrollEndRef} />
+      </div>
+
+      <div className='App-operationBoard'>
+        {/* <FlushAnimation /> */}
+      </div>
+
+      <textarea ref={textareaRef} ></textarea>
+      <button type="button" onClick={addLog}>送信</button>
+
+      <div className='App-note'>
+        ctrl+enter
+      </div>
     </div>
   );
 }
